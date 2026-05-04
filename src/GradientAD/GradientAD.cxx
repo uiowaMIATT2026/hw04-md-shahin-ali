6a90a2e5cc91ced0ce6a187cfa3e6782275295e3
Md Shahin Ali <mali25@engr.uiowa.edu>
2026-05-03 22:25:45 -0500

    docs: add file header comment to GradientAD.cxx

--- src/GradientAD/GradientAD.cxx
+++ src/GradientAD/GradientAD.cxx
@@ -1,3 +1,7 @@
+// GradientAD - Gradient anisotropic diffusion filter for 3D NIFTI images
+// Author: Md Shahin Ali (mali25)
+// ECE:5490 HW04
+
 // GradientAD.cxx
 // ECE:5490 HW04 - Gradient Anisotropic Diffusion Filter
 // Author: Md Shahin Ali (mali25)
